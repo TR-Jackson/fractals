11df8d1cc90fa2cc10b377ba0d1444ee55f18a23
Tom <tom.jackson.tc@btinternet.com>
2022-05-23 19:11:32 +0100

    appears to work for a quadratic

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ class Polynomial():
     # Given an order, generate random roots or use given roots
     def __init__(self, order=False, roots=False, coefficients=False):
         if order:
+            # bad, needs to take into account complex root pairs
             self.order = order
             for i in range(order):
                 self.roots.append(complex(random(), random()))
@@ -18,24 +19,24 @@ class Polynomial():
             self.order = len(roots) 
             self.roots = roots
         elif coefficients:
-            self.order = len(coefficients) 
+            self.order = len(coefficients)-1
             self.coefficients = coefficients
 
     def genCoefficients(self):
-        coefficients = [1]
+        coefficients = [1.0]
         for i in range(self.order):
-            coefficients.append((-1)**(i+1) * self.calcSumOfProducts(self.roots, i))
+            coefficients.append(((-1)**(i+1) * self.calcSumOfProducts(self.roots, i)).real)
         self.coefficients = coefficients
 
     def genDerivative(self):
         derivCoes = []
-        for i in range(len(self.coefficients)-1, 0, -1):
-            derivCoes.append(self.coefficients[self.order-i-1] * i)
+        for i in range(len(self.coefficients)-1):
+            derivCoes.append((len(self.coefficients)-1-i)*self.coefficients[i])
         self.derivative = Polynomial(coefficients=derivCoes)
 
     def calc(self, x):
         result = self.coefficients[0]
-        for c in range(1, self.order):
+        for c in range(1, self.order+1):
             result = result * x + self.coefficients[c]
         return result
     
@@ -58,29 +59,41 @@ class Polynomial():
             return sum
 
 def newtonRaphson(x, poly):
-    if poly.calcDerivative(x) == 0:
+    fdash = poly.calcDerivative(x)
+    f = poly.calc(x)
+    if fdash == 0:
         return x, True # Won't find root
-    return x - poly.calc(x)/poly.calcDerivative(x), False
+    return x - f/fdash, False
 
 def findRoot(x0, poly):
     iterCount = 0
-    result = x0
+    result = poly.calc(x0)
+    root = x0
     diverges = False
-    while not math.isclose(poly.calc(result).real, 0, abs_tol=1e-2) and not math.isclose(poly.calc(result).imag, 0, abs_tol=1e-2) and iterCount < 100 and not diverges:
-        result, diverges = newtonRaphson(result, poly) 
+    while not (math.isclose(result.real, 0, abs_tol=1e-2) and math.isclose(result.imag, 0, abs_tol=1e-2)) and iterCount < 100 and not diverges:
+        root, diverges = newtonRaphson(root, poly)
+        result = poly.calc(root)
         iterCount = iterCount + 1
     if iterCount == 100 or diverges:
+        print(iterCount)
         return False # Root not found
     else:
-        return True # Root found
+        return root # Root found
 
 
-poly = Polynomial(roots=[complex(1,0), complex(2,0), complex(3,0), complex(4,0), complex(217,0)])
+poly = Polynomial(roots=[complex(1,0), complex(2,0)])
 poly.genCoefficients()
+print(poly.roots)
+print(poly.coefficients)
 poly.genDerivative()
+print(poly.derivative.coefficients)
 points = []
-# for x in range(-100, 100):
-#     for y in range(-100, 100):
-#         if findRoot(complex(x,y), poly):
-#             points.append(x,y)
+for x in range(-10, 10):
+    for y in range(-10, 10):
+        root = findRoot(complex(x,y), poly)
+        if root:
+            for r in poly.roots:
+                if math.isclose(root.real, r.real, abs_tol=1e-1) and math.isclose(root.imag, r.imag, abs_tol=1e-1):
+                    points.append([x,y,r])
+print(points)
 
